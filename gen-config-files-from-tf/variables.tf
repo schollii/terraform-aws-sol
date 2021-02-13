c9fe5710eba0f736b41f17275a73849058df39bb
Oliver L Schoenborn <oliver.schoenborn@gmail.com>
2021-02-12 21:08:02 -0500

    Update variables.tf

--- gen-config-files-from-tf/variables.tf
+++ gen-config-files-from-tf/variables.tf
@@ -3,7 +3,7 @@ variable "stack_id" {
   description = "An identifier for the whole stack being provisioned in AWS"
 }
 
-variable "k8s_namespace" {
+variable "k8s_ns" {
   type    = string
   default = "**"
   description = "The k8s namespace for the resources being provisioned. Defaults to all/any."
@@ -14,9 +14,21 @@ variable "deployment_roots" {
   description = "List of folders in which the configuration files are located. Must contain _templates_ folder."
 }
 
-variable "template_in" {
+variable "tpl_name" {
   type = string
   description = "Name of template file to look for in all deployment_root folders"
+}
+
+variable "tpl_auto_root" {
+  type = string
+  description = "Filename prefix for root auto files"
+  default = "auto-root"
+}
+
+variable "tpl_auto" {
+  type = string
+  description = "Filename prefix for non-root auto files"
+  default = "auto"
 }
 
 variable "template_vars" {
